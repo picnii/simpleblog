9a8716439e006f0b4bcff2d853729f681cd49ac5
picnii <sompop@picnii.me>
2013-09-13 15:10:24 +0700

    test Controller

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -21,6 +21,12 @@ class SiteController extends Controller
 		);
 	}
 
+	public function actionTest()
+	{
+		echo "Test Controller";
+	}
+
+
 	/**
 	 * This is the default 'index' action that is invoked
 	 * when an action is not explicitly requested by users.
